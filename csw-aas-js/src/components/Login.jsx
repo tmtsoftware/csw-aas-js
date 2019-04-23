bead2f8fc1187634930160408f9b25498008781c
Pritam Kadam <kpritam@thoughtworks.com>
2019-04-24 01:32:54 +0530

    Use hooks.

--- csw-aas-js/src/components/Login.jsx
+++ csw-aas-js/src/components/Login.jsx
@@ -1,28 +1,19 @@
-import React from 'react'
-import PropTypes from 'prop-types'
-import WithContext from './context/WithContext'
+import React, { useContext } from 'react'
+import { AuthContext } from './context/AuthContext'
 
 /**
  * React component which renders Login button.
  */
-export class Login extends React.Component {
-  render() {
-    return (
-      <button
-        id='aas-login'
-        onClick={
-          // Call to this.props.login method is responsible for resolving and instantiating AAS server
-           this.props.login
-        }>
-        Login
-      </button>
-    )
-  }
+export const Login = props => {
+  const { login } = useContext(AuthContext)
+  return (
+    <button
+      id='aas-login'
+      // Call to props.login method is responsible for resolving and instantiating AAS server
+      onClick={login}>
+      Login
+    </button>
+  )
 }
 
-Login.propTypes = {
-  // WithContext utility provides login method as a prop.
-  login: PropTypes.func,
-}
-
-export default WithContext(Login)
+export default Login
--- csw-aas-js/src/components/authentication/CheckLogin.jsx
+++ csw-aas-js/src/components/authentication/CheckLogin.jsx
@@ -1,6 +1,6 @@
-import React from 'react'
+import { useContext } from 'react'
 import PropTypes from 'prop-types'
-import { Consumer } from '../context/AuthContext'
+import { AuthContext } from '../context/AuthContext'
 
 /**
  * React component which renders children if authenticated and error if unauthenticated
@@ -9,14 +9,11 @@ import { Consumer } from '../context/AuthContext'
  * @param error (optional prop) can be react components or html element which will be rendered
  * if user is not authenticated
  */
-const CheckLogin = ({ children, error }) => (
-  <Consumer>
-    {({ auth }) => {
-      if (!auth) return error
-      return auth.isAuthenticated() ? children : error || null
-    }}
-  </Consumer>
-)
+const CheckLogin = ({ children, error }) => {
+  const { auth } = useContext(AuthContext)
+  if (!auth) return error
+  return auth.isAuthenticated() ? children : error || null
+}
 
 CheckLogin.propTypes = {
   children: PropTypes.node,
