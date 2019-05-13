58e4373d62b2f2fee4b8634c84d1290367ace11f
Poorva Gokhale <poorvasgokhale@gmail.com>
2019-05-13 17:32:57 +0530

    Add type declaration for csw-aas-js library to enable usage from typescript application (#1)
    *  typings for csw-aas-js
    
    * Change version number for testing
    
    * #000: Poorva: Add typings for csw-aas-js
    
    * #000: Poorva: Copy typings to dist folder.
    
    * #000: Poorva: Remove example-ts (test project for typings) and upgrade version

---
+++ csw-aas-js/src/typings/csw-aas-js.d.ts
@@ -0,0 +1,71 @@
+import * as React from 'react'
+import {
+  KeycloakProfile,
+  KeycloakPromise,
+  KeycloakResourceAccess,
+  KeycloakRoles,
+  KeycloakTokenParsed,
+} from 'keycloak-js'
+
+
+export interface Auth {
+  logout?: (options?: any) => KeycloakPromise<void, void>,
+  token?: () => string | undefined,
+  tokenParsed?: () => KeycloakTokenParsed | undefined ,
+  realmAccess?: () => KeycloakRoles | undefined , // todo: should this be called realmRoles?
+  resourceAccess?: () => KeycloakResourceAccess | undefined, // todo: should this be called resourceRoles?
+  loadUserProfile?: () => KeycloakPromise<KeycloakProfile, void>,
+  isAuthenticated?: () => boolean | undefined,
+  hasRealmRole?: (role: string) => boolean,
+  hasResourceRole: (role: string, resource?: string) => boolean,
+}
+
+interface IAuthContext {
+  auth: Auth | null,
+  login: () => void,
+  logout: () => void
+}
+
+export const AuthContext: React.Context<IAuthContext>
+
+export const Consumer: React.Consumer<IAuthContext>
+
+export interface AuthConfig {
+  realm: string,
+  clientId: string
+}
+
+export interface AuthContextProviderProps {
+  config: AuthConfig,
+  children: React.ReactNode
+}
+
+export const AuthContextProvider: React.FunctionComponent<AuthContextProviderProps>
+
+export interface CheckLoginProps {
+  children?: React.ReactNode,
+  error?: React.ReactNode
+}
+
+export const CheckLogin: React.FunctionComponent<CheckLoginProps>
+
+export interface ClientRoleProps {
+  clientRole: string,
+  client?: string,
+  children?: React.ReactNode,
+  error?: React.ReactNode
+}
+
+export const ClientRole: React.FunctionComponent<ClientRoleProps>
+
+export const Login: React.ComponentType
+
+export const Logout: React.ComponentType
+
+export interface RealmRoleProps {
+  realmRole: string,
+  children?: React.ReactNode,
+  error?: React.ReactNode
+}
+
+export const RealmRole: React.FunctionComponent<RealmRoleProps>
