f8bde53f2bce76112fbd3fe853e901a78430da3f
Scala Steward <me@scala-steward.org>
2019-09-17 17:13:01 +0200

    Update webdrivermanager to 3.7.1

--- project/Libs.scala
+++ project/Libs.scala
@@ -11,7 +11,7 @@ object Libs {
 
   val `selenium-java`     = "org.seleniumhq.selenium"                  % "selenium-java"      % "3.141.59"
   val `embedded-keycloak` = "com.github.tmtsoftware.embedded-keycloak" %% "embedded-keycloak" % "0.1.6"
-  val `webdrivermanager`  = "io.github.bonigarcia"                     % "webdrivermanager"   % "3.6.2"
+  val `webdrivermanager`  = "io.github.bonigarcia"                     % "webdrivermanager"   % "3.7.1"
 
   val `scala-async` = dep("org.scala-lang.modules" %% "scala-async"  % "0.10.0")
   val `scalajs-dom` = dep("org.scala-js"           %%% "scalajs-dom" % "0.9.7")
