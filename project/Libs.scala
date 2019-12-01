13165170e0eaddae0efe0ebc4dad6e5d2f724bcd
Scala Steward <me@scala-steward.org>
2019-12-01 04:37:36 +0100

    Update csw-config-server, ... to v1.0.0-RC4

--- project/Libs.scala
+++ project/Libs.scala
@@ -21,7 +21,7 @@ object Libs {
 object CSW {
   val Version: String = {
     val env = sys.env ++ sys.props
-    env.getOrElse("CSW_VERSION", "master-SNAPSHOT")
+    env.getOrElse("CSW_VERSION", "v1.0.0-RC4")
   }
 
   val `csw-location-server` = "com.github.tmtsoftware.csw" %% "csw-location-server" % Version
