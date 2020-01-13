6e237cee5b5abce0087636dbcce1c66a8af98193
Mushtaq Ahmed <mushtaq.a@gmail.com>
2020-01-13 13:59:37 +0530

    adapt to flat json from csw

--- csw-aas-js/src/components/AASResolver.ts
+++ csw-aas-js/src/components/AASResolver.ts
@@ -8,33 +8,31 @@ import {Config} from '../config/configs'
 export const resolveAAS: () => Promise<string | null> = async () => {
   // URL constant which contains location service end point for resolving AAS server
 
-  const URL = `${Config['location-server-url']}/post-endpoint`
+  const URL = `${Config['location-server-url']}/post-endpoint`;
 
   const resolveRequest = {
-    Resolve: {
-      connection: {
-        prefix: "csw.AAS",
-        componentType: 'service',
-        connectionType: 'http',
-      },
-      within: '5 seconds',
+    _type: "Resolve",
+    connection: {
+      prefix: "csw.AAS",
+      componentType: 'service',
+      connectionType: 'http',
     },
-  }
+    within: '5 seconds',
+  };
 
   const response = await window.fetch(URL, {
     method: 'POST',
     headers: {
-      'Content-Type': 'application/json',
-      'Accept': 'application/json'
+      'Content-Type': 'application/json'
     },
     redirect: 'follow', // manual, *follow, error
     referrer: 'no-referrer', // no-referrer, *client
     body: JSON.stringify(resolveRequest),
-  })
+  });
 
   if (response.status === 200) {
-    const jsonResponse = await response.json()
-    return jsonResponse[0].HttpLocation.uri
+    const jsonResponse = await response.json();
+    return jsonResponse[0].uri
   }
   return null
-}
+};
